f25e9900c058a42146fcc65600c26f459cea6cc8
yinyinayescm <60867210+yinyinayescm@users.noreply.github.com>
2022-09-06 11:33:45 +0630

    using vue compoment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="app">
-    Hello Vue Typescript
+  <div>
+    <h1>Hello Vue Typescript</h1>
+    <p>Name : {{ name }}</p>
+    <p>Age : {{ age }}</p>
+    <button class="btn primary-btn" @click="changeName('Smith')">Change Name</button>
+    <button class="btn primary-btn ml-20" @click="changeAge()">Change Age</button>
   </div>
 </template>
 
@@ -9,8 +13,22 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
+  data() {
+    return {
+      name: 'John',
+      age: 25 as number | string,
+    }
+  },
+  methods: {
+    changeName(newName: string) {
+      this.name = newName
+    },
+    changeAge() {
+      // this.age = 30
+      this.age = "30"
+    }
   }
+
 });
 </script>
 
